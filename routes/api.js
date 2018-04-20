5f4077b9ab3da4973997d7fc8c3bc366bd181f13
KariNarhinen <kari@inia.fi>
2018-04-20 09:25:27 +0300

    initial backend

---
+++ routes/api.js
@@ -0,0 +1,22 @@
+// Dependencies
+var express = require('express');
+var router = express.Router();
+
+// Models
+var TimesModel = require('../models/times');
+var UsersModel = require('../models/users');
+var ProjectsModel = require('../models/projects');
+// Routes
+router.get('/timeaction', function(req,res){
+    res.send('API responded')
+});
+/*TimesModel.methods(['get', 'put', 'post', 'delete']);
+TimesModel.register(router, '/timesheet');
+UsersModel.methods(['get', 'put', 'post', 'delete']);
+UsersModel.register(router, '/user');
+ProjectsModel.methods(['get', 'put', 'post', 'delete']);
+ProjectsModel.register(router, '/project');
+*/
+
+// Return Router
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require ('express');
+var mongoose = require('mongoose');
+var bodyParser  = require('body-parser');
+
+// TODO fix the MongoDB connect
+// if you want to activate connection to MongoDB
+// remove the following comment 
+// MongoDD
+//mongoose.connect('mongodb://localhost/timesheet');
+mongoose.connect('mongodb://worktimeuser:yrM9iE9zMOBKILdKzjKE@ds151259.mlab.com:51259/worktime');
+
+//Express
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+// Create a simple router to be able to see this is working 
+
+//Routes
+app.use('/api', require('./routes/api'));
+
+// Start server
+app.listen (3000);
+console.log('server is listening on port 3000');
