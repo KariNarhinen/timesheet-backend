d499c8d73aeebe7604b0d4a85d45748e944dc811
KariNarhinen <kari@inia.fi>
2018-04-20 10:20:27 +0300

    Take mongodb into use

--- routes/api.js
+++ routes/api.js
@@ -7,16 +7,18 @@ var TimesModel = require('../models/times');
 var UsersModel = require('../models/users');
 var ProjectsModel = require('../models/projects');
 // Routes
+/*
 router.get('/timeaction', function(req,res){
     res.send('API responded')
 });
-/*TimesModel.methods(['get', 'put', 'post', 'delete']);
+*/
+
+TimesModel.methods(['get', 'put', 'post', 'delete']);
 TimesModel.register(router, '/timesheet');
 UsersModel.methods(['get', 'put', 'post', 'delete']);
 UsersModel.register(router, '/user');
 ProjectsModel.methods(['get', 'put', 'post', 'delete']);
 ProjectsModel.register(router, '/project');
-*/
 
 // Return Router
 module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,23 @@ var express = require ('express');
 var mongoose = require('mongoose');
 var bodyParser  = require('body-parser');
 const PORT = process.env.PORT || 3000
-
+const DBUSER = process.env.DBUSER || ''
+const DBPW = process.env.DBPW || ''
 // TODO fix the MongoDB connect
 // if you want to activate connection to MongoDB
 // remove the following comment 
 // MongoDD
-//mongoose.connect('mongodb://localhost/timesheet');
-//mongoose.connect('mongodb://user:password@ds151259.mlab.com:51259/worktime');
 
-//Express
+console.log("DBUSER is : "+ DBUSER + "!");
+if (DBUSER=='') {
+    console.log("Connection to localhost DB");
+    mongoose.connect('mongodb://localhost/timesheet');
+} else {
+    console.log("Connection to testing DB");
+    mongoose.connect(`mongodb://${ DBUSER}:${ DBPW }@ds151259.mlab.com:51259/worktime`);
+}
+
+//Express0
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
